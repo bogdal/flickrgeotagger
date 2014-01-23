341a5a0565e0be784e68fa479c73fe7aa0f03a76
Adam Bogdal <adam@bogdal.pl>
2014-01-23 23:39:18 +0100

    Revert "Serve static files through wsgi"
    This reverts commit d926da9191aa705e843d11ea63db05c0a5339b9e.

--- setup.py
+++ setup.py
@@ -21,8 +21,7 @@ setup(
         'django-sekizai==0.7',
         'django-geoip-utils==0.1.1',
         'django-dropboxchooser-field==0.0.2',
-        'dj-database-url>=0.2.2',
-        'dj-static>=0.0.5',
+        'dj_database_url>=0.2.2',
     ],
     entry_points={
         'console_scripts': [
