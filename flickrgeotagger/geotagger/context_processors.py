2b50a914e3aa0d161df9d6b2d23a75575df0dbe1
Adam Bogdal <adam@bogdal.pl>
2014-02-19 23:57:27 +0100

    Use appropriate name of variable

--- flickrgeotagger/geotagger/context_processors.py
+++ flickrgeotagger/geotagger/context_processors.py
@@ -3,5 +3,5 @@ from django.conf import settings
 
 def social_media_buttons(request):
     return {
-        'SHOW_ADDTHIS_BUTTONS': getattr(settings, 'SHOW_GITHUB_BUTTONS'),
+        'SHOW_ADDTHIS_BUTTONS': getattr(settings, 'SHOW_ADDTHIS_BUTTONS'),
         'SHOW_GITHUB_BUTTONS': getattr(settings, 'SHOW_GITHUB_BUTTONS')}
