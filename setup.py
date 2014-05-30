85ff68dbf39efa7a7e0be97a667ea056f2587eb3
Adam Bogdal <adam@bogdal.pl>
2014-05-30 09:16:09 +0200

    Use appropriate event name

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
         'python-social-auth>=0.1.24',
         'django-sekizai==0.7',
         'django-geoip-utils==0.1.1',
-        'django-dropboxchooser-field>=0.0.4',
+        'django-dropboxchooser-field>=0.0.5',
         'dj_database_url>=0.2.2',
     ],
     entry_points={
